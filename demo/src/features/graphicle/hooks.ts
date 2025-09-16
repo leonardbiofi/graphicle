680c4ee1d40017068674b18775bb5385a6609d81
Leonard Gable <leonard@biofi.ch>
2025-09-16 08:45:43 +0200

    feat: implement force layout

--- demo/src/features/graphicle/hooks.ts
+++ demo/src/features/graphicle/hooks.ts
@@ -1,13 +1,15 @@
-import { useSyncExternalStore } from "react";
-import { ObservableStyle } from "./observableStyle";
+import { useEffect, useMemo, useRef, useSyncExternalStore } from "react";
+import * as d3 from "d3-force";
+import { Simulation } from "d3-force";
+import type { Node } from "@graphicle/base";
+import { throttle } from "@tanstack/pacer";
+
 import { useGraphicleStore } from "@/store/graphicleStore";
 import { useForceLayoutStore } from "@/store/layoutStore";
-import { useEffect } from "react";
-import * as d3 from "d3-force";
-import type { Node } from "@graphicle/base";
 import { useGraphicle } from "@/components/GraphicleProvider";
-import { useMemo, useRef } from "react";
-import { Simulation } from "d3-force";
+
+import { ObservableStyle } from "./observableStyle";
+
 export function useObservableStyle<TStyle extends object>(
   styleStore: ObservableStyle<TStyle>
 ): [TStyle, ObservableStyle<TStyle>] {
@@ -54,6 +56,46 @@ export function useForceLayout() {
       fx: undefined,
     }));
 
+    const onTick = throttle(
+      () => {
+        if (!graphicleRef.current) return;
+
+        const nodes = graphicleRef.current.store.getNodes();
+
+        const nextNodes = nodes.map((n, i) => {
+          if (simulationNodes[i]) {
+            const { x, y } = simulationNodes[i];
+            const dragging = draggingNodeRef.current?.id === n.id;
+            if (dragging) {
+              // Setting the fx/fy properties of a node tells the simulation to
+              // "fix" the node at that position and ignore any forces that would
+              // normally cause it to move.
+              //
+              // The node is still part of the simulation, though, and will push
+              // other nodes around while the simulation runs.
+              // @ts-expect-error FIXME: need better typescript handling
+              simulationNodes[i].fx = n.position.x;
+              // @ts-expect-error FIXME: need better typescript handling
+              simulationNodes[i].fy = n.position.y;
+
+              return { ...n, position: draggingNodeRef.current?.position };
+            } else {
+              delete simulationNodes[i].fx;
+              delete simulationNodes[i].fy;
+            }
+
+            return { ...n, position: { x: x ?? 0, y: y ?? 0 } };
+          }
+
+          return { ...n };
+        });
+
+        // @ts-expect-error FIXME: need better typescript handling
+        setNodes(nextNodes); // trigger state update}, { wait: 100 });
+      },
+      { wait: 50 }
+    );
+
     simulationRef.current = d3
       .forceSimulation(simulationNodes)
       .force(
@@ -70,46 +112,7 @@ export function useForceLayout() {
       .alphaDecay(0.015) // slower cooling
       .alpha(0.3)
       .restart()
-      // .alpha(1)
-      // .alphaDecay(0.02)
-
-      // adjust to your canvas size
-      .on("tick", () => {
-        const nodes = graphicleRef.current.store.getNodes();
-
-        const nextNodes = nodes.map((n, i) => {
-          if (simulationNodes[i]) {
-            const { x, y } = simulationNodes[i];
-            const dragging = draggingNodeRef.current?.id === n.id;
-            if (dragging) {
-              // Setting the fx/fy properties of a node tells the simulation to
-              // "fix" the node at that position and ignore any forces that would
-              // normally cause it to move.
-              //
-              // The node is still part of the simulation, though, and will push
-              // other nodes around while the simulation runs.
-              simulationNodes[i].fx = n.position.x;
-              simulationNodes[i].fy = n.position.y;
-              // simulationNodes[i].x = draggingNodeRef.current?.position.x;
-              // simulationNodes[i].y = draggingNodeRef.current?.position.y;
-              return { ...n, position: draggingNodeRef.current?.position };
-            } else {
-              delete simulationNodes[i].fx;
-              delete simulationNodes[i].fy;
-            }
-
-            return { ...n, position: { x: x ?? 0, y: y ?? 0 } };
-          }
-
-          return { ...n };
-        });
-
-        setNodes(nextNodes); // trigger state update
-      });
-    // const nextNodes = nodes.map((n) => ({
-    //   ...n,
-    //   position: { x: positions[n.id].x, y: positions[n.id].y },
-    // }));
+      .on("tick", onTick);
 
     return () => {
       if (simulationRef.current) {
@@ -118,26 +121,6 @@ export function useForceLayout() {
       }
     }; // stop when unmounted or disabled
   }, [isForceActive, graphicleRef, setNodes]);
-
-  // useEffect(() => {
-  //   const d3Nodes = d3NodesRef.current;
-  //   if (!d3Nodes || d3Nodes.length === 0) return;
-
-  //   // Sync updated positions for selected nodes
-  //   nodes.forEach((n) => {
-  //     const d3Node = d3Nodes.find((d) => d.id === n.id);
-  //     if (d3Node && n.position) {
-  //       d3Node.x = n.position.x;
-  //       d3Node.y = n.position.y;
-  //       d3Node.data = { ...n };
-  //       // Optionally fix the node
-  //       if (n.selected) {
-  //         d3Node.fx = n.position.x;
-  //         d3Node.fy = n.position.y;
-  //       }
-  //     }
-  //   });
-  // }, [nodes]);
 
   return dragEvents;
 }
