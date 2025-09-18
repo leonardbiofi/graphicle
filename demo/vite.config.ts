6664073f5531507dc3a86d26e666c6c3bbf1fa95
Leonard <leonard.gable@outlook.com>
2025-09-18 10:18:50 +0200

    feat: attempt to optimize

--- demo/vite.config.ts
+++ demo/vite.config.ts
@@ -16,12 +16,6 @@ const config = defineConfig({
     }),
     viteReact(),
   ],
-  server: {
-    headers: {
-      "Cross-Origin-Opener-Policy": "same-origin",
-      "Cross-Origin-Embedder-Policy": "require-corp",
-    },
-  },
 });
 
 export default config;
