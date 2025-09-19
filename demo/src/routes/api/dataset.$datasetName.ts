3cc4441971e65dab0f1a3e5ca7b32fe27bc204b8
Leonard <leonard.gable@outlook.com>
2025-09-19 11:11:48 +0200

    fix: test some netlify configuration

--- demo/src/routes/api/dataset.$datasetName.ts
+++ demo/src/routes/api/dataset.$datasetName.ts
@@ -1,22 +1,22 @@
 import { createServerFileRoute } from "@tanstack/react-start/server";
 // import { promises as fs } from "fs";
-import * as fs from "node:fs";
-import { join } from "node:path";
+// import * as fs from "node:fs";
+// import { join } from "node:path";
 
 // import path from "path";
 import { json } from "@tanstack/react-start";
 
-// import miserablejson from "@/data/miserables.json";
-// import genejson from "@/data/genes.json";
-// import worldcupjson from "@/data/worldcup.json";
-// import circlejson from "@/data/circle.json";
+import miserablejson from "@/data/miserables.json";
+import genejson from "@/data/genes.json";
+import worldcupjson from "@/data/worldcup.json";
+import circlejson from "@/data/circle.json";
 
-// const datasets: Record<string, object> = {
-//   miserables: miserablejson,
-//   genes: genejson,
-//   circle: circlejson,
-//   worldcup: worldcupjson,
-// };
+const datasets: Record<string, object> = {
+  miserables: miserablejson,
+  genes: genejson,
+  circle: circlejson,
+  worldcup: worldcupjson,
+};
 // import { fileURLToPath } from "url";
 
 // __dirname equivalent in ESM
@@ -36,18 +36,18 @@ async function readDataset(filename: string): Promise<any> {
   try {
     // const filePath = path.join("src", "data", `${filename}.json`);
 
-    const fileContents = await fs.promises.readFile(
-      join(process.cwd(), `data/${filename}.json`),
-      {
-        encoding: "utf-8",
-      }
-    );
+    // const fileContents = await fs.promises.readFile(
+    //   join(process.cwd(), `data/${filename}.json`),
+    //   {
+    //     encoding: "utf-8",
+    //   }
+    // );
 
-    // const fileContents = datasets[filename];
-
+    const fileContents = datasets[filename];
+    return fileContents;
     // console.log("fileConten:", fil);
 
-    return JSON.parse(fileContents);
+    // return JSON.parse(fileContents);
   } catch (error) {
     console.error(`Failed to read dataset file, ${error}`);
     throw new Error(`Dataset "${filename}" not found or invalid JSON.`);
--- demo/vite.config.ts
+++ demo/vite.config.ts
@@ -3,7 +3,7 @@ import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 import viteReact from "@vitejs/plugin-react";
 import viteTsConfigPaths from "vite-tsconfig-paths";
 import tailwindcss from "@tailwindcss/vite";
-// import { viteStaticCopy } from "vite-plugin-static-copy";
+import { viteStaticCopy } from "vite-plugin-static-copy";
 const config = defineConfig({
   plugins: [
     // this is the plugin that enables path aliases
@@ -11,18 +11,19 @@ const config = defineConfig({
       projects: ["./tsconfig.json"],
     }),
     tailwindcss(),
+    viteStaticCopy({
+      targets: [
+        {
+          src: "data/**/*.json",
+          dest: "data",
+        },
+      ],
+    }),
     tanstackStart({
       target: "netlify",
       customViteReactPlugin: true,
     }),
-    // viteStaticCopy({
-    //   targets: [
-    //     {
-    //       src: "data/*.json",
-    //       dest: "data/*.json",
-    //     },
-    //   ],
-    // }),
+
     viteReact(),
   ],
 });
